d797356458108e843e5c36a8f89352f576e30511
ngurahbas <ngurahbaskara@gmail.com>
2025-07-13 23:20:55 +0800

    spring boot init

---
+++ src/main/kotlin/app/sbplayground2/Sbplayground2Application.kt
@@ -0,0 +1,11 @@
+package app.sbplayground2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Sbplayground2Application
+
+fun main(args: Array<String>) {
+    runApplication<Sbplayground2Application>(*args)
+}
---
+++ src/test/kotlin/app/sbplayground2/Sbplayground2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.sbplayground2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Sbplayground2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
