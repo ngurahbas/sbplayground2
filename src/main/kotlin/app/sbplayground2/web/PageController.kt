6f4f005959bd5c7b774233d96f8917b6bcf7e246
ngurahbas <ngurahbaskara@gmail.com>
2025-07-18 23:54:42 +0800

    revise mapping strategy

--- src/main/kotlin/app/sbplayground2/web/PageController.kt
+++ src/main/kotlin/app/sbplayground2/web/PageController.kt
@@ -3,13 +3,11 @@ package app.sbplayground2.web
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping("/")
 class PageController {
 
-    @GetMapping
+    @GetMapping("/")
     fun home(model: Model): String {
         model.addAttribute("title", "Welcome to SB Playground")
         model.addAttribute("message", "Hello from Thymeleaf!")
