20ac1d856be8d20f0aa26d580ec857925761fd24
ngurahbas <ngurahbaskara@gmail.com>
2025-07-21 13:27:26 +0800

    renanme

--- src/main/kotlin/app/sbplayground2/web/HomeController.kt
+++ src/main/kotlin/app/sbplayground2/web/HomeController.kt
@@ -1,15 +1,13 @@
 package app.sbplayground2.web
 
-import org.springframework.http.ResponseEntity
-import org.springframework.security.core.annotation.AuthenticationPrincipal
 import app.sbplayground2.domain.user.AuthenticatedUser
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @Controller
-class PageController {
+class HomeController {
 
     @GetMapping("/")
     fun home(model: Model, @AuthenticationPrincipal principal: AuthenticatedUser?): String {
@@ -25,21 +23,5 @@ class PageController {
         }
         
         return "home" // This will resolve to src/main/resources/templates/home.html
-    }
-}
-
-@RestController
-class ApiController {
-    
-    @GetMapping("/api/user-info")
-    fun getUserInfo(@AuthenticationPrincipal principal: AuthenticatedUser): ResponseEntity<Map<String, Any>> {
-        val userInfo = mapOf(
-            "name" to principal.name,
-            "idType" to principal.idType.name,
-            "idValue" to principal.idValue,
-            "idSource" to principal.idSource.name
-        )
-        
-        return ResponseEntity.ok(userInfo)
     }
 }
